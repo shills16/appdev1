c0947187874b221c6b00dad1497826e1cbae203b
shills16 <shielamiclat10@gmail.com>
2024-10-12 19:48:09 -0700

    I'm done completing the section of rendering lists using keys

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,15 +1,20 @@
 import "./App.css"
 
-function App() {
   const products = [
-    { title: "Cabbage", id: 1 },
-    { title: "Garlic", id: 2 },
-    { title: "Apple", id: 3}
+    { title: "Cabbage", isFruit: false, id: 1 },
+    { title: "Garlic", isFruit: false, id: 2 },
+    { title: "Apple", isFruit: true, id: 3}
   ];
 
+export default function ShoppingList() {
   const listItems = products.map(product => 
-    <li key={product.id}>
-      {product.title}
+    <li 
+      key={product.id}
+      style={{
+        color: product.isFruit ? "magenta" : "darkgreen"
+      }}
+    >
+        {product.title}
     </li>
   );
 
@@ -21,4 +26,3 @@ function App() {
   );
 }
 
-export default App
