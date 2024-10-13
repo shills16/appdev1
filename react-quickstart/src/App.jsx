8b4fc8b27b57e13b333e17ee36012ed70e18cc79
shills16 <shielamiclat10@gmail.com>
2024-10-12 20:29:20 -0700

    I'm done completing the section of updating screen (counters that update together)

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,13 +1,27 @@
-import MyButton from "./MyButton.jsx"
+import { useState } from "react"
 
 export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <>
-      <h1>Counters that update separately</h1>
-      <MyButton />
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
       <br />
       <br />
-      <MyButton />
+      <MyButton count={count} onClick={handleClick} />
     </>
   );
+}
+
+function MyButton({ count, onClick }) {
+  return (
+    <button onClick = {onClick}>
+      Clicked {count} times
+    </button>
+  )
 }
