a7a3c750a99caf3da953f6fe01728b7aa6e356bf
shills16 <shielamiclat10@gmail.com>
2024-10-12 19:59:41 -0700

    I'm done completing the section of updating screen (clicking the button will increment the counter)

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css"
+import { useState } from "react";
 
 function MyButton() {
+  const [count, setCount] = useState(0);
+
   function handleClick() {
-    alert("You clicked me!");
+    setCount(count + 1);
   }
   
   return (
     <>
       <h1>Welcome to my app</h1>
       <button onClick={handleClick}>
-        Click me
+        Clicked {count} times
       </button>
     </>
   );
