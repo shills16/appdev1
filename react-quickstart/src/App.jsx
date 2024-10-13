d34b6fd5033cd03f1d2c38030ec67d026f579c71
shills16 <shielamiclat10@gmail.com>
2024-10-12 19:36:12 -0700

    I'm done completing the rendering lists using the map function to transform an arraay of products into an array of items

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,20 +1,24 @@
 import "./App.css"
-import AdminPanel from "./AdminPanel";
-import LoginForm from "./LoginForm";
 
 function App() {
-  let isLoggedIn = tfalse;
+  const products = [
+    { title: "Cabbage", id: 1 },
+    { title: "Garlic", id: 2 },
+    { title: "Apple", id: 3}
+  ];
+
+  const listItems = products.map(product => 
+    <li key={product.id}>
+      {product.title}
+    </li>
+  );
 
   return (
     <>
       <h1>Welcome to my app</h1>
-      {isLoggedIn ? (
-        <AdminPanel />
-      ) : (
-        <LoginForm />
-      )}
+      <ul>{listItems}</ul>
     </>
-  )
+  );
 }
 
 export default App
